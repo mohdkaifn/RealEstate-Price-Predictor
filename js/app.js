11dca05d1031caf4a3e2bb511d7f1b9b5aa2a4ae
Mohammed Kaif <114495145+mohdkaifn@users.noreply.github.com>
2024-05-29 11:47:48 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var selectElement = document.getElementById("uiLocations");
+var selectedValue = selectElement.value;
+
+// Use the selected value as needed
+console.log("hi");
+console.log(selectedValue);
